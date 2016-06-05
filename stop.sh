559d888a363faec048eb86e1d7fcb8a8d20f9267
Sho Tanaka <twinkle.over.the.sky@gmail.com>
2016-06-05 09:12:24 +0900

    stagingの起動、停止用にスクリプトを修正

--- stop.sh
+++ stop.sh
@@ -3,13 +3,18 @@
 # 引数に年度を渡して起動
 # deploy-scripts/start.sh 2016
 
-YEAR=${1}
+DEPLOY_TARGET=${1}
+YEAR=${2}
 
-CONFIG_FILE=/opt/workspace/deploy-scripts/${YEAR}/config.sh
+CONFIG_FILE=/opt/workspace/deploy-scripts/${YEAR}/config-${DEPLOY_TARGET}.sh
 
 if [ ! -f $CONFIG_FILE ]; then
     echo '引数が無効です'
     exit
 fi
 
-kill `cat /var/run/pyconjp/gunicorn${YEAR}.pid`
+if [ ${DEPLOY_TARGET} = "production" ]; then
+  kill `cat /var/run/pyconjp/gunicorn${YEAR}.pid`
+elif [ ${DEPLOY_TARGET} = "staging" ]; then
+  kill `cat /var/run/pyconjp/gunicorn${YEAR}-${DEPLOY_TARGET}.pid`
+fi
